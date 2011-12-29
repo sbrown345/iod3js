b711e8aecc63b4b2434dfc9b3e2e6536e92f09cf
Sander van Rossen <sander.vanrossen@gmail.com>
2011-12-29 13:08:21 +0100

    fixed small mistake

--- neo/workdirectory/base/gl2progs/interaction.fs
+++ neo/workdirectory/base/gl2progs/interaction.fs
@@ -19,20 +19,18 @@ uniform vec4 				u_viewOrigin;
 uniform vec4 				u_diffuseColor;        
 uniform vec4 				u_specularColor;        
       
-vec3 FresnelSchlick( vec3 specularColor, vec3 L, vec3 H ) {   
-    return specularColor + ( 1.0f - specularColor ) * pow(1.0f - clamp( dot( L, H ), 0.0, 1.0 ), 5 );   
+vec3 FresnelSchlick( vec3 specularColor, vec3 V, vec3 H ) {   
+    return specularColor + ( 1.0f - specularColor ) * pow(1.0f - clamp( dot( V, H ), 0.0, 1.0 ), 5 );   
 }
     
-vec4 lightingModel( vec4 diffuseTerm, vec3 specularTerm, vec3 L, vec3 N, vec3 H ) {   
+vec4 lightingModel( vec4 diffuseTerm, vec3 specularTerm, vec3 L, vec3 V, vec3 N, vec3 H ) {   
 #if 1
 	// half-lambertian blinn-phong lighting model   
 	float NdotL = ( dot( N, L ) + 0.5 ) / ( 1.0 + 0.5 );  
-	NdotL *= NdotL;
-
-	//float NdotH = pow( dot( N, H ), 2 );  
+	NdotL *= NdotL; 
  
 	vec4 diffuseLighting  = max( diffuseTerm  * NdotL, 0.0);   
-	vec3 specularLighting = max( FresnelSchlick(specularTerm, L, H), 0.0);  
+	vec3 specularLighting = max( FresnelSchlick(specularTerm, V, H), 0.0);  
  
 	return diffuseLighting + vec4( specularLighting.rgb, 1.0 );      
 #else 
@@ -63,15 +61,14 @@ void main( void ) {
 	vec4 diffuse = texture2D( u_diffuseTexture, var_TexDiffuse )
 					* u_diffuseColor; 
 
+	// this is the calculation from the specular fallof texture
 	vec3 temp;
 	temp.x = dot(var_TangentBitangentNormalMatrix[0], H);
 	temp.y = dot(var_TangentBitangentNormalMatrix[1], H);
 	temp.z = dot(var_TangentBitangentNormalMatrix[2], H);
-
 	float _specular = dot(normalize(temp), N);
-
-	//_specular = dot(_specular, N);
-	vec3 falloff = pow(max((_specular - 0.75) * 4, 0.0), 2.0) * u_specularColor.rgb;
+	vec3 falloff = pow(max((_specular - 0.75) * 4, 0.0), 2.0)
+					* u_specularColor.rgb;
 
 	// compute the specular term
 	vec3 specular = (2.0 * texture2D( u_specularTexture, var_TexSpecular ).rgb * falloff)
@@ -84,7 +81,7 @@ void main( void ) {
 	vec3 lightProjection	= texture2DProj( u_lightProjectionTexture, var_TexLight.xyw ).rgb; 
 
 	// compute lighting model
-    vec4 color = lightingModel( diffuse, specular, L, N, H );  
+    vec4 color = lightingModel( diffuse, specular, L, V, N, H );  
 	color.rgb *= lightProjection; 
 	color.rgb *= lightFalloff; 
 	color.rgb *= var_Color.rgb; 
